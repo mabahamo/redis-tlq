9e8681ad59d7b505b7b80f6468a6dc44735d68f6
Manuel Bahamondez Honores <manuel@bahamondez.com>
2014-03-12 14:21:26 -0300

    custom max memory

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,6 @@
 
 # whethere to prevent redis from binding to 127.0.0.1
 default[:redis][:dont_bind] = false
+
+# prevent redis from ever using 400MB of memory
+default[:redis][:max_memory] = 419430400
