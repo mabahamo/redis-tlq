1f5cfa9a9026ebab045aec60df9d32ecc1cf5fb5
Manuel Bahamondez Honores <manuel@bahamondez.com>
2014-03-12 16:27:24 -0300

    redis without bind

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "redis-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Installs redis from rwky's ppa"
-version           "0.0.8"
+version           "0.0.9"
 
 recipe "redis-tlq", "Installs redis"
 
